e7fa172fb649d1b7cbc63842e34045b01465113b
MehulAgg26 <21BMA003@nith.ac.in>
2022-03-05 14:37:28 +0530

    Redirected URLs

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path("january", views.jan),
     # path("february", views.feb),
-    path("<month>", views.monthly_challenge)
+    path("<int:month>", views.monthly_challenge_by_no), # Order also matters here we first wanna check for int then str
+    path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound , HttpResponseRedirect
+
+monthly_challenges_dict = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 min every day!",
+    "march": "Learn Django everday for 20 min!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 min every day!",
+    "june": "Learn Django everday for 20 min!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 min every day!",
+    "september": "Learn Django everday for 20 min!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 min every day!",
+    "december": "Learn Django everday for 20 min!"
+}
 
 
 # Create your views here.
-# def jan(request):
-#     return HttpResponse("Eat no meat for the entire month!")
-# def feb(request):
-#     return HttpResponse("Walk for at least 20 min every day!")
+def monthly_challenge_by_no(request, month):
+    months_list = list(monthly_challenges_dict.keys())
+    # Logic is month as int is the argument we are getting here and we will put the value of redirect month using the
+    # basic python list index value and method
+    if month>len(months_list):
+        return HttpResponseNotFound("Invalid Month!! Try Again")
+    redirect_month = months_list[month-1]
+    return HttpResponseRedirect("/challenges/"+redirect_month)
 
 
 def monthly_challenge(request, month):
-    challenge_text = ""
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    if month == "february":
-        challenge_text = "Walk for at least 20 min every day!"
-    return HttpResponse()
+    try:
+        challenge_text = monthly_challenges_dict[month]
+    except KeyError:
+        return HttpResponseNotFound('This month is not supported')
+    return HttpResponse(challenge_text)
